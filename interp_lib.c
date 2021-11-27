de8d1e7c83d857b472b632707041a69028444ca2
jar <jar-evans@github.com>
2021-11-27 11:37:15 +0100

    Add Newton basis
    Implements issue #1

--- interp_lib.c
+++ interp_lib.c
@@ -51,8 +51,45 @@ static PyObject* py_lagrange(PyObject* self, PyObject* args) {
 }
 
 static PyObject* py_newton(PyObject* self, PyObject* args) {
-    int y = 10;
-    return Py_BuildValue("i", y);
+
+    PyObject* X, *Y;
+    double x;
+    int i,j,k;
+    double y = 0;
+
+    if(!PyArg_ParseTuple(args, "O|O|d", &X, &Y, &x)) {
+        return NULL;
+    }
+
+    if (PyObject_Length(X) != PyObject_Length(Y)) {
+        return NULL;
+    }
+
+    int len = PyObject_Length(X);
+
+    double difference_table[len][len];
+
+    double c_X[len];
+    double c_Y[len];
+
+    for (k = 0; k < len; k++) {
+        c_X[k] = (double)PyLong_AsLong(PyList_GetItem(X, k));
+        c_Y[k] = (double)PyLong_AsLong(PyList_GetItem(Y, k));
+        difference_table[0][k] = c_Y[k];
+    }
+
+    for (i = 1; i < len; i++) {
+        for (j = 0; j < len-i; j++) {
+            difference_table[i][j] = (difference_table[i-1][j+1] - difference_table[i-1][j])/(c_X[i+j] - c_X[j]);
+        }     
+    }
+
+    for (k = len-1; k >= 0; k--) {
+        y *= (x - c_X[k]);
+        y += difference_table[k][0];
+    }
+
+    return Py_BuildValue("d", y);
 
 }
 
--- interp_test.py
+++ interp_test.py
@@ -5,25 +5,4 @@ import numpy as np
 import matplotlib.pyplot as plt 
 from time import time
 
-def f(x):
-    return x*x
-
 if __name__ == "__main__":
-    X = np.array([-1,0,1])
-    Y = f(X).tolist()
-    X = X.tolist()
-    xs = np.arange(-1, 1, 0.5)
-    Z = []
-
-    print(interp.newton())
-   
-    start = time()
-    for x in xs:
-        Z.append(interp.lagrange(X,Y,x+0.8))
-    end = time()
-
-    print(f"Runtime: {end - start}[s]")
-
-    plt.plot(X,Y)
-    plt.plot(xs,Z)
-    plt.show()

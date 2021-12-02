2f08a412a6ca91deb8ed1d2c0dd1f8593222eaca
jar <jar-evans@github.com>
2021-12-02 19:21:59 +0100

    Adding monomial basis
    Implementing issue #2

--- interp_lib.c
+++ interp_lib.c
@@ -93,8 +93,133 @@ static PyObject* py_newton(PyObject* self, PyObject* args) {
 
 }
 
+static PyObject* py_monomial(PyObject* self, PyObject* args) {
+
+    PyObject* X, *Y;
+    double x;
+    int i,j,k;
+    double y = 0;
+
+    if(!PyArg_ParseTuple(args, "O|O|d", &X, &Y, &x)) {
+        return NULL;
+    }
+
+    if (PyObject_Length(X) != PyObject_Length(Y)) {
+        return NULL;
+    }
+
+    int len = PyObject_Length(X);
+
+    //Initialise Vandermonde
+    double V[len][len];
+
+    double c_X[len];
+    double c_Y[len];
+
+    for (k = 0; k < len; k++) {
+        c_X[k] = (double)PyLong_AsLong(PyList_GetItem(X, k));
+        c_Y[k] = (double)PyLong_AsLong(PyList_GetItem(Y, k));
+    }
+    
+    for (i = 0; i < len; i++) {
+        for (j = 0; j < len; j++) {
+            V[i][j] = 1;
+            for (k = 0; k < j; k++) {
+                V[i][j] *= c_X[i];
+            }
+        }
+    }
+
+    double matrix[len][len];
+	for (i = 0; i < len; i++) {
+		for (j = 0; j < 2 * len; j++) {
+
+            if (j < len) {
+                matrix[i][j] = V[i][j];
+            }
+
+			if (j == (i + len)) {
+				matrix[i][j] = 1;
+                continue;
+            }
+
+            matrix[i][j] = 0;
+        }
+    }
+
+    double temp;
+
+	for (i = len - 1; i > 0; i--) {
+
+		if (matrix[i - 1][0] < matrix[i][0])
+            for (int j = 0; j < 2 * len; j++) {
+                temp = matrix[i][j];
+                matrix[i][j] = matrix[i - 1][j];
+                matrix[i - 1][j] = temp;
+            }
+	}
+
+
+	for (i = 0; i < len; i++) {
+        if(matrix[i][i] == 0.0)
+        {
+            printf("No inverse!\n");
+            return Py_BuildValue("d", 1);
+        }
+		for (j = 0; j < len; j++) {
+
+			if (j != i) {
+				temp = matrix[j][i] / matrix[i][i];
+				for (k = 0; k < 2 * len; k++) {
+					matrix[j][k] -= matrix[i][k] * temp;
+				}
+			}
+		}
+	}
+
+	for (i = 0; i < len; i++) {
+		temp = matrix[i][i];
+		for (j = 0; j < 2 * len; j++) {
+			matrix[i][j] = matrix[i][j] / temp;
+		}
+	}
+
+    double inv[len][len];
+
+	for (i = 0; i < len; i++) {
+		for (j = len; j < 2 * len; j++) {
+			inv[i][j-len] = matrix[i][j];
+		}
+	}
+
+    double coeffs[len];
+
+    for (i = 0; i < len; i++) {
+        coeffs[i] = 0;
+        for (j = 0; j<len; j++) {
+            coeffs[i] += inv[i][j]*c_Y[j];
+        }
+    }
+    y = 0;
+    float w = 0;
+    for (i = len-1; i >=0; i--) {
+        w = coeffs[i];
+        for (j = 0; j<i; j++) {
+            w *= x;
+        }
+        y += w;
+    }
+
+    return Py_BuildValue("d", y);
+ 
+}
+
+
+
+
 static PyMethodDef interp_methods[] = {{"lagrange", py_lagrange, METH_VARARGS, NULL}, 
                                        {"newton", py_newton, METH_VARARGS, NULL},
+                                       {"monomial", py_monomial, METH_VARARGS, NULL},
                                        {NULL, NULL, 0, NULL}};
 
 static struct PyModuleDef moduleDef = {PyModuleDef_HEAD_INIT, "interp_lib", NULL, -1, interp_methods};
--- interp_test.py
+++ interp_test.py
@@ -6,3 +6,13 @@ import matplotlib.pyplot as plt
 from time import time
 
 if __name__ == "__main__":
+    X = [0, np.pi/2, np.pi]
+    Y = [0, 1, 0]
+    x = np.arange(0, np.pi+0.1, 0.1).tolist()
+    y = []
+    for sample in x:
+        y.append(interp.monomial(X, Y, sample))
+
+    plt.scatter(X, Y)
+    plt.plot(x, y)
+    plt.show()
